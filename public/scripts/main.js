03fe9eabeb3befc9970375cf3faaa8f34bda4839
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-06-18 19:29:19 +0200

    Homepage responsive

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -45,56 +45,56 @@ if (document.body.classList.value === "body--home") {
     // 2.2 CONTENT ARRAYS
     const problemsContent = [{
             title: "Slow Traffic Updates",
-            text: "One of the main causes for traffic congestion is poor anticipation. As it stands today, by the time traffic congestion is detected and communicated to road users, the situation has more often than not already aggravated."
+            text: "One of the main causes for traffic congestion is poor anticipation, which is why we must foster ways to identify and communicate it faster."
         },
         {
-            title: "Undetected Road Anomalies",
-            text: "Roadworks, accidents, and deviations happen to be a part of urban mobility. And while they may sometimes be inevitable, we should certainly strive to manage them with more reactive, efficient, and secure methods."
+            title: "Road Anomalies",
+            text: "Roadworks, accidents, and deviations may sometimes be inevitable, but we can strive to manage them with more reactive, efficient, and secure methods."
         },
         {
             title: "Traffic Congestion",
-            text: "We've all been stuck in a line of cars that stretches into the horizon at some point. To this day, traffic congestion causes major issues related to mental health, time efficiency, and economic productivity."
+            text: "To this day, traffic congestion causes major issues related to mental health, time efficiency, and economic productivity."
         },
         {
             title: "Unreliable Public Transport",
-            text: "One of the greatest assets of urban cities is ironically also a major source of frustration. Many of us are dependent on public transport, and its service quality and timeliness still leave a lot to be desired."
+            text: "Many of us are dependent on public transport, and its service quality and timeliness still leave a lot to be desired."
         },
         {
-            title: "Inefficient Delivery Services",
-            text: "In an economy where virtually everything can be purchased through a screen, we need to find a better way to get online orders from store to door, for the benefit of consumers, sellers, and carriers alike."
+            title: "Inefficient Delivery",
+            text: "In today's digital economy, we need to find better and faster ways to get online orders from store to door."
         },
         {
             title: "Excess Fuel Consumption",
-            text: "There are only so many natural resources available to us, and we are racing towards a depletion of our raw materials. Urban traffic in particular can't sustain its current fuel consumption for much longer."
+            text: "There are only so many natural resources available to us, and we are racing towards a depletion of our raw materials."
         },
         {
             title: "Increased Pollution",
-            text: "Our planet is the most valuable thing we have, yet we don't treat it as such. In urban cities more than anywhere else, it's about time that we start making a real effort to reduce our greenhouse gas emissions."
+            text: "Our planet is the most valuable thing we have, yet we don't treat it as such judging by our greenhouse gas emissions."
         },
     ];
     const solutionsContent = [{
             title: "Road Anomaly Detection",
-            text: "Using the latest geolocation technology and complex image recognition algorithms, GaliGo automatically detects and locates road anomalies such as holes in the road, accidents, and roadworks."
+            text: "GaliGo automatically detects and locates road anomalies such as holes in the road, accidents, and roadworks."
         },
         {
             title: "Traffic Signal Analysis",
-            text: "Traffic congestion issues tends to occur repeatedly in the same areas, which is why GaliGo uses its AI-powered technology to identify miscoordination between traffic signals in urban communities along with possible improvements."
+            text: "GaliGo uses its AI-powered technology to identify miscoordination between traffic signals in urban communities."
         },
         {
             title: "Smart Itinerary Planning",
-            text: "We all share the same roads to get from point A to point B, and GaliGo takes this into account in a real-time navigation system that considers all road users in order to propose itineraries that them to their destination via the fastest and most efficient route."
+            text: "GaliGo takes a whole new approach to proposing the best and most efficient itineraries to road users."
         },
         {
             title: "Real-time Traffic Updates",
-            text: "Unexpected things happen on the road, and while GaliGo can't promise to eliminate them entirely, it sure can offer to identify and communicate them faster and more accurately than it has ever been done before."
+            text: "GaliGo can't promise to eliminate accidents and other inconveniences entirely, but it sure can identify and communicate them faster than ever before."
         },
         {
             title: "Public Transport Tracking",
-            text: "GaliGo's technology is, among other things, deployed on public transport vehicles, thus allowing users to know exactly where their next bus is at this very moment and when it will reach them at their stop."
+            text: "GaliGo's technology allows citizens to locate their next bus at all times and with unprecedented precision."
         },
         {
             title: "Delivery Tracking",
-            text: "With GaliGo, the days of missing deliveries due to unpunctual carriers or impractical schedules are over: Users can now know well in advance when they can expect their online orders to be delivered and can follow them throughout the entire delivery process."
+            text: "With GaliGo, the days of missing deliveries due to unpunctual carriers or impractical schedules are over."
         }
     ];
     // 2.3 EVENT LISTENERS
