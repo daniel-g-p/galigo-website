8998d69de49b4c4854336aa6dd58728aa21cc98a
Daniel Giustini Perez <danielgiustiniperez@gmail.com>
2021-06-25 13:43:37 +0200

    Project completed

---
+++ public/forms.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST["submit-newsletter"])) {
+    $name = $_POST["firstName"];
+    $mailFrom = $_POST["email"];
+    $mailTo = "marketing@3daerospace.eu";
+    $subject = "NEW GALIGO NEWSLETTER SUBSCRIBER";
+    $message = "Subscriber Details: \n\nName: ".$name."\nEmail: ".$mailFrom;
+    $headers = "From: ".$mailFrom;
+    mail($mailTo, $subject, $message, $headers);
+    header("Location: /");
+}
+
+if(isset($_POST["submit-contact"])) {
+    $name = $_POST["name"];
+    $mailFrom = $_POST["email"];
+    $subject = $_POST["subject"];
+    $text = $_POST["message"];
+    $mailTo = "marketing@3daerospace.eu";
+    $message = "New Message on the GaliGo Website: \n\nName: ".$name."\nEmail: ".$mailFrom."\n\nMessage:\n".$text;
+    $headers = "From: ".$mailFrom;
+    mail($mailTo, $subject, $message, $headers);
+    header("Location: /");
+}
